7174f24c1841bd04fb448e35fd88df586126e737
zohebk <zoheb_kerawalla@yahoo.com>
2023-05-17 00:15:33 -0700

    updated folder structure

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 export GRADIO_SERVER_NAME=0.0.0.0 
 export GRADIO_SERVER_PORT="$PORT"
-cp cdnindex.html /app/.heroku/python/lib/python3.11.3/site-packages/gradio/templates/cdn/index.html
-cp frontendindex.html /app/.heroku/python/lib/python3.11.3/site-packages/gradio/templates/frontend/index.html
+cp cdnindex.html /app/.heroku/python/lib/python3.11/site-packages/gradio/templates/cdn/index.html
+cp frontendindex.html /app/.heroku/python/lib/python3.11/site-packages/gradio/templates/frontend/index.html
